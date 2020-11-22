2a741fe4a349aece4e9cca85d41b3b089b32e5e5
Scott Shebby <scott@intercreate.io>
2020-11-21 18:56:25 -0800

    use modified built version

--- examples/src/index.js
+++
@@ -1,5 +0,0 @@
-import './index.css'
-
-import('./lib').then(function (mod) {
-  (mod && mod.default)()
-})
--- examples/src/lib.js
+++
@@ -1,3 +0,0 @@
-export default function () {
-  document.getElementById('root').innerText = 'JS is working!'
-}
